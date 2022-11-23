69b6c4c761faca3d659c6b07c9e71b7b99790405
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2022-11-23 15:15:30 +0700

    update

---
+++ Oneteam/src/main/java/HDT/Oneteam/OneteamApplication.java
@@ -0,0 +1,13 @@
+package HDT.Oneteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneteamApplication.class, args);
+	}
+
+}
---
+++ Oneteam/src/test/java/HDT/Oneteam/OneteamApplicationTests.java
@@ -0,0 +1,13 @@
+package HDT.Oneteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
